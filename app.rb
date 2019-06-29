dffb28238ae6bd4fe45f2b270d189326778ac2ff
Travis Exline <t.exline22@gmail.com>
2019-06-29 15:46:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    piglat = PigLatinizer.new
+    @piglatin = piglat.piglatinize(params[:user_input])
+    erb :results
+  end
 end
